1f10975853e20c02e3f49d9fd70fc9a68ea24266
Fathimath Thazeera <fathimaththazeera@gmail.com>
2021-12-17 16:03:46 +0530

    controller

--- demo 2/src/main/java/com/example/demo/controller/BookController.java
+++ demo 2/src/main/java/com/example/demo/controller/BookController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Collection;
-//import java.util.HashMap;
-//import java.util.Map;
+
 
 @RestController
 @RequestMapping("/book")
