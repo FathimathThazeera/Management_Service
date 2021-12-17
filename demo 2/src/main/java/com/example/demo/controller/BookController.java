17d444ada85f76722ff8d76776e3e285f3f59900
Fathimath Thazeera <fathimaththazeera@gmail.com>
2021-12-17 15:44:04 +0530

    commit

--- demo 2/src/main/java/com/example/demo/controller/BookController.java
+++ demo 2/src/main/java/com/example/demo/controller/BookController.java
@@ -1,27 +1,31 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.Book;
+import com.example.demo.service.BookService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+//import java.util.HashMap;
+//import java.util.Map;
 
 @RestController
 @RequestMapping("/book")
 public class BookController {
 
-    private Map<Long, Book> map = new HashMap<>();
+    //private Map<Long, Book> map = new HashMap<>();
+    @Autowired
+    BookService bookService;
 
     @GetMapping("/all")
     public ResponseEntity<Collection<Book>> getAll() {
-        return ResponseEntity.ok(map.values());
+        return ResponseEntity.ok(bookService.getMap().values());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Book> getById(@PathVariable long id) {
-        Book book = map.get(id);
+        Book book = bookService.getById(id);
         if (book == null) {
             return ResponseEntity.notFound().build();
         }
@@ -30,29 +34,32 @@ public class BookController {
 
     @PostMapping("/insert")
     public ResponseEntity<Void> insert(@RequestBody Book book) {
-        if (map.containsKey(book.getId())) {
+        boolean res = bookService.insert(book);
+        if (res) {
+            return ResponseEntity.ok().build();
+        } else {
             return ResponseEntity.badRequest().build();
         }
-        map.put(book.getId(), book);
-        return ResponseEntity.ok().build();
     }
 
     @PutMapping("/update")
     public ResponseEntity<Void> update(@RequestBody Book book) {
-        if (!map.containsKey(book.getId())) {
+        boolean res = bookService.update(book);
+        if (!res) {
             return ResponseEntity.badRequest().build();
+        } else {
+            return ResponseEntity.ok().build();
         }
-        map.put(book.getId(), book);
-        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
-        if (!map.containsKey(id)) {
+        boolean res = bookService.delete(id);
+        if (!res) {
+            return ResponseEntity.badRequest().build();
+        } else {
             return ResponseEntity.ok().build();
         }
-        map.remove(id);
-        return ResponseEntity.ok().build();
-    }
 
+    }
 }
